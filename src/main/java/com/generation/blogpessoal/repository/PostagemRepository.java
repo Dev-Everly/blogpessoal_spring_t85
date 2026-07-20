d90ed1e9e28758ac0805665714cdb9d344d109e9
Dev-Everly <everlysantos98@gmail.com>
2026-07-20 08:22:53 -0300

    :sparkles: Finalização do CRUD - Recurso Postagem

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,10 +1,15 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
-
+// Primeiro parametro <Model, Tipo Id>  Segundo parametro
 public interface PostagemRepository extends JpaRepository <Postagem, Long>{
 	
-
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+	// SELECT * FROM tb_postagens WHERE titulo LIKE "% ? %";
 }
